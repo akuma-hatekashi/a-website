b6d0355f1a05ef28825c7d96a0bebcbfdd5883d5
akuma hatekashi <83667192+akuma-hatekashi@users.noreply.github.com>
2021-05-14 13:16:36 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,46 @@
-const button = document.querySelector('button.add');
-const input = document.querySelector('input.new');
-const itemsList = document.querySelector('ul.items');
-const totalText = document.querySelector('span.total');
+const captiontext = document.getElementById('caption-text');
+const checkAllButton = document.getElementById('check-all-button');
+const uncheckAllButton = document.getElementById('uncheck-all-button');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-button.addEventListener('click', function() {
-  const newItem = document.createElement('li');
-  newItem.innerHTML = input.value;
-  itemsList.appendChild(newItem);
-  totalText.innerHTML = `(${itemsList.childElementCount} items)`;
-  input.value = '';
+function updateSummary() {
+  let numChecked = 0;
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      numChecked++;
+    }
+  });
+  captionText.innerHTML = `${numChecked} of ${checkboxes.length} checked`;
+  if (numChecked === 0) {
+    checkAllButton.disabled = false;
+    uncheckAllButton.disabled = true;
+  } else {
+    uncheckAllButton.disabled = false;
+  }
+  if (numChecked === checkboxes.length) {
+    checkAllButton.disabled = true;
+  }
+}
+
+checkAllButton.addEventListener('click', function() {
+  checkboxes.forEach(function(checkbox) {
+    checkbox.checked = true;
+    checkbox.closest('tr').className = 'checked';
+  });
+  updateSummary();
+});
+
+uncheckAllButton.addEventListener('click', function() {
+  checkboxes.forEach(function(checkbox) {
+    checkbox.checked = false;
+    checkbox.closest('tr').className = '';
+  });
+  updateSummary();
+});
+
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function(event) {
+    checkbox.closest('tr').className = checkbox.checked ? 'checked' : '';
+    updateSummary();
+  });
 });
